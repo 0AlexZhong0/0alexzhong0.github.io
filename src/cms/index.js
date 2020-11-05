e726f8aadc63e950d2729baefab3986b86da8e0c
alex.zhong <0alexzhong0@gmail.com>
2020-11-05 15:25:25 +1300

    Configure to run netlify cms locally

--- src/cms/index.js
+++ src/cms/index.js
@@ -3,5 +3,17 @@ import CMS from 'netlify-cms-app';
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
 
+CMS.init();
+
 CMS.registerPreviewTemplate('pages', PagePreview);
 CMS.registerPreviewTemplate('posts', PostPreview);
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', (user) => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
+      });
+    }
+  });
+}
